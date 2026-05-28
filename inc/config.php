f3d4ce8129b76c2a39ccda9ca89fa8b9f7902ced
a-ruel <a.ruel@example.com>
2026-05-28 10:06:47 +0000

    Ajout de config.php (contenant les identifiants BDD)

---
+++ inc/config.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gestion_heures');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+try {
+    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
